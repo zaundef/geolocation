00abdb6f0a371dd3bfa9963c82568e3792405952
Vincent ELLAMA <vincent.ellama@gmail.com>
2014-03-12 14:15:16 +0100

    first commit

---
+++ Gruntfile.js
@@ -0,0 +1,21 @@
+module.exports = function(grunt) {
+	require('load-grunt-tasks')(grunt);
+
+	var gruntTasksPath = './grunt_tasks';
+
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+		autoprefixer: require(gruntTasksPath+'/autoprefixer'),
+		clean: require(gruntTasksPath+'/clean'),
+		cssmin: require(gruntTasksPath+'/cssmin'),
+		connect: require(gruntTasksPath+'/connect'),
+		copy: require(gruntTasksPath+'/copy'),
+		mkdir: require(gruntTasksPath+'/mkdir'),
+		jade: require(gruntTasksPath+'/jade'),
+		stylus: require(gruntTasksPath+'/stylus'),
+		uglify: require(gruntTasksPath+'/uglify'),
+		watch: require(gruntTasksPath+'/watch')
+	});
+
+	grunt.registerTask('init', ['mkdir:base']);
+};
