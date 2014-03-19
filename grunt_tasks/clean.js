69055251318814351550446f796c26bda7e3b93b
Vincent ELLAMA <vincent.ellama@gmail.com>
2014-03-19 10:14:44 +0100

    grunt_tasks deleted

--- grunt_tasks/clean.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-	all: {
-		src: ['build', 'demo']
-	}
-}
--- grunt_tasks/cssmin.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-	build: {
-		expand: true,
-		cwd: 'build',
-		src: ['**/*.css'],
-		dest: 'build',
-		ext: '.min.css'
-	}
-};
